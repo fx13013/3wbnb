7d4727ab92d477bd17e6cbb36918d4eaf3520594
fx13013 <r_carlos013@hotmail.com>
2019-12-04 23:23:04 +0100

    Ad Controller

---
+++ src/Controller/AdController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @return Response
+     */
+    public function show(Ad $ad){
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class HomeController extends Controller {
 
     /**
@@ -14,40 +15,11 @@ class HomeController extends Controller {
      * @return void
      */
     public function home(){
-        $prenoms = ["Felix" => 37, "Alyss" => 35, "Meyline" => 8];
-
         return $this->render(
-            'home.html.twig',
-            [ 
-                'title' => "Au revoir tout le monde",
-                'age' => 12,
-                'tableau' => $prenoms
-            ]
-        );
-    }
-
-
-    /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     * Montre la page qui dit hello 
-     *
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0){
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom' => $prenom,
-                'age' => $age
-            ]
+            'home.html.twig'
         );
     }
 }
-
-
-
 
 
 ?>

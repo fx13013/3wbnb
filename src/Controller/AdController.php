5be78a6e633c41eed51e7970368608e40dc5d840
fx13013 <r_carlos013@hotmail.com>
2019-12-06 16:04:06 +0100

    Formulaires, messages flash, include twig

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -24,6 +28,41 @@ class AdController extends AbstractController
         ]);
     }
 
+
+    /**
+     * Permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $manager){
+        $ad = new Ad();
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
     /**
      * Permet d'afficher une seule annonce
      * 
@@ -36,4 +75,6 @@ class AdController extends AbstractController
             'ad' => $ad
         ]);
     }
+
+    
 }
